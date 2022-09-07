1150d3062c61a8294afd9994c6adeaef422f6e00
Olesya Pasichnyk <olesya2011@hotmail.no>
2022-09-07 17:10:44 +0200

    Experiment 1 up to step 6 done

---
+++ eclipselink/experiment-1/src/main/java/no/hvl/dat250/jpa/assignment2/Todo.java
@@ -0,0 +1,17 @@
+package no.hvl.dat250.jpa.assignment2;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String summary;
+    private String description;
+}
